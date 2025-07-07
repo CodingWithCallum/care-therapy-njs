266f5870c444784787c1d0fd03e18268cb5931e0
Callum Barry <codingwithcallum@gmail.com>
2025-07-07 15:42:25 +0200

    fix

--- src/app/OurStory/page.tsx
+++ src/app/OurStory/page.tsx
@@ -6,7 +6,7 @@ export default function OurStory() {
     <section className="py-25 mt-10">
         <div className="max-w-7xl mx-auto sm:px-10 md:px-12 lg:px-5 md:flex-row gap-16 justify-items-center">
             <h1 className="mt-6 text-3xl/tight sm:text-4xl/tight md:text-5xl/tight xl:text-6xl/tight font-bold text-gray-900">
-                <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-700 from-30% via-blue-400 via-50% to-teal-600">CARE Therapy's Story</span>
+                <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-700 from-30% via-blue-400 via-50% to-teal-600">CARE Therapys Story</span>
             </h1>
         </div>
         <div className="max-w-7xl mx-auto sm:px-10 md:px-12 lg:px-5 md:flex-row gap-16">
